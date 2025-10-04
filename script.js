3cc6253065577c4eccccb280286b17972a4afe26
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2025-10-04 12:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const openBtn = document.getElementById('openModal');
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementById('closeModal');
+
+openBtn.onclick = () => {
+    modal.classList.add('show');
+};
+
+closeBtn.onclick = () => {
+    modal.classList.remove('show');
+};
+
+// Click outside modal-content to close
+window.onclick = (e) => {
+    if (e.target === modal) {
+        modal.classList.remove('show');
+    }
+};
